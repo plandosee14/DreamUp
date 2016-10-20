13e62a43995546e44accded269a1d63283ff1635
kosta <kosta@192.168.0.103>
2016-10-20 20:38:45 +0900

    대맹이왔어염?>

--- DreamUp/src/com/dreamup/dao/cart/CartDAO.java
+++ DreamUp/src/com/dreamup/dao/cart/CartDAO.java
@@ -1,17 +1,24 @@
 package com.dreamup.dao.cart;
 
 import java.util.List;
-import com.dreamup.dto.cart.*;
+
+import com.dreamup.dto.cart.CartDTO;
+import com.dreamup.dto.project.ProjectDTO;
 
 public class CartDAO {
-	
-	public boolean insertCart(CartDTO cart){
+
+	public boolean insertCart(ProjectDTO project) {
+
 		return false;
 	}
-	public List<CartDTO> selectAllCart(String id){
+
+	public List<CartDTO> selectAllCart(String id) {
+
 		return null;
 	}
-	public boolean deleteCart(int pNo){
+
+	public boolean deleteCart(int pNo) {
+
 		return false;
 	}
 
--- DreamUp/src/com/dreamup/dao/cart/reply/ReplyDAO.java
+++ DreamUp/src/com/dreamup/dao/cart/reply/ReplyDAO.java
@@ -1,5 +1,28 @@
 package com.dreamup.dao.cart.reply;
 
-public class ReplyDAO {
+import java.util.List;
 
+import com.dreamup.dto.cart.reply.ReplyDTO;
+
+public class ReplyDAO {
+	
+public boolean insertReply(ReplyDTO comment){
+		
+		return false;
+	}
+	
+	public ReplyDTO selectReply(int coNo){
+		ReplyDTO reply = new ReplyDTO();
+		
+		return reply;
+	}
+	
+	public List<ReplyDTO> selectAllReply(){
+		return null;
+	}
+	
+	public boolean deleteReply(int coNo){
+		
+		return false;
+	}
 }
