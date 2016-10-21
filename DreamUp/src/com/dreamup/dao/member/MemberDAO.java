be70fc588436ec64b87effad7c5432b45dc1d8cd
KOSTA-078 <KOSTA-078@192.168.0.125>
2016-10-21 16:11:10 +0900

    맴버다오

--- DreamUp/src/com/dreamup/dao/member/MemberDAO.java
+++ DreamUp/src/com/dreamup/dao/member/MemberDAO.java
@@ -2,6 +2,7 @@ package com.dreamup.dao.member;
 
 import java.net.StandardSocketOptions;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.eclipse.jdt.internal.compiler.batch.Main;
 
@@ -15,6 +16,36 @@ public class MemberDAO {
 	public MemberDAO() {
 		sqlMap = SqlMapConfig.getSqlMapInstance();
 	}
+	
+	public boolean insert(MemberDTO member){
+		return false;
+	}
+	
+	public boolean delete(String id){
+		return false;
+	}
+	
+	public boolean update(MemberDTO member){
+		return false;
+	}
+	
+	public MemberDTO selectMember(String id){
+		return null;
+	}
+	
+	public List<MemberDTO> listMember(){
+		return null;
+	}
+	
+	public boolean LoginCheck(String id, String pass){
+		return false;
+	}
+	
+	public String findId(String name,String email){
+		return null;
+	}
+	
+	
 
 	public MemberDTO select() {
 		MemberDTO member;
