ee17e2654ac187d4a9527e5fd3034a51eccc4c94
윤영미 <@192.168.0.30>
2016-10-29 16:45:38 +0900

    멤버 다오, sql 수정(다운)

--- DreamUp/sql/member.sql
+++ DreamUp/sql/member.sql
@@ -1,31 +1,32 @@
 drop table member;
 create table member(
-	M_No			integer,					-- 멤버 번호			
-	M_Id			varchar2(30) 	primary key, -- 멤버 아이디
-	M_Password		varchar2(150)	not null, -- 멤버 비밀번호
-	M_Email			varchar2(150)	not null, -- 멤버 이메일 , 중복검사 할것
-	M_Name			varchar2(30)    not null, --멤버 이름
-	M_Date			date		not null, -- 가입일
+   M_No         integer,               -- 멤버 번호         
+   M_Id         varchar2(30)    primary key, -- 멤버 아이디
+   M_Password      varchar2(150)   not null, -- 멤버 비밀번호
+   M_Email         varchar2(150)   not null, -- 멤버 이메일 , 중복검사 할것
+   M_Name         varchar2(30)    not null, --멤버 이름
+   M_Date         date      not null, -- 가입일
     --회원가입시 입력 받는 컬럼 (INSERT)
-	 
-	M_Phone			varchar2(13), --1.후원할때 or 2.프로젝트 등록시 입력
-	M_Address		varchar2(255), -- 1.후원할때 배송지 정보 or 2.프로젝트 등록시 주소정보 입력
-	M_Post			varchar2(20), -- 1.후원할때 배송지 정보 or 2.프로젝트 등록시 주소정보 입력
-	
-	M_BankName		varchar2(30), -- 환불시 은행명
-	M_Account		varchar2(20), -- 환불시 계좌번호
-	--후원정보 입력시 입력 받는 컬럼 (UPDATE)
-	
-	m_SupportingCount		varchar2(20), -- 내가 후원하는 프로젝트 수 총합계 (후원시 +1)
-	m_SupportiedCount		varchar2(20) -- 내 프로젝트 총 후원합계 (환불시 -1)
+    
+   M_Phone         varchar2(13), --1.후원할때 or 2.프로젝트 등록시 입력
+   M_Address      varchar2(255), -- 1.후원할때 배송지 정보 or 2.프로젝트 등록시 주소정보 입력
+   M_Post         varchar2(20), -- 1.후원할때 배송지 정보 or 2.프로젝트 등록시 주소정보 입력
+   
+   M_BankName      varchar2(30), -- 환불시 은행명
+   M_Account      varchar2(20), -- 환불시 계좌번호
+   --후원정보 입력시 입력 받는 컬럼 (UPDATE)
+   
+   m_SupportingCount      int, -- 내가 후원하는 프로젝트 수 총합계 (후원시 +1)
+   m_supportedCount      int -- 내 프로젝트 총 후원합계 (환불시 -1)
 );
-	
-		insert into member
-		(m_no, m_id, m_password, m_email, m_name, m_date)
-		values
-		(member_seq.nextval, 'test1', '1234', 'kancho33@naver.com', '최용석', sysdate);
-		
-select * from member     
+   
+      insert into member
+      (m_no, m_id, m_password, m_email, m_name, m_date)
+      values
+      (member_seq.nextval, 'test1', '1234', 'kancho33@naver.com', '최용석', sysdate);
+      
+select * from member   
+select * from member where m_no = 6
 
 select m_id from member where m_email='kancho33@naver.com' and m_name ='기호창';
 
@@ -41,7 +42,19 @@ delete * from member where M_ID='damang';
 
 drop sequence member_seq;
 create sequence member_seq
-	start with 1
-	increment by 1
-	nocycle
-	nocache;
+   start with 1
+   increment by 1
+   nocycle
+   nocache;
+   
+   
+         update member
+      set
+      m_supportingCount=m_supportingCount+1
+      where m_id= 'test1' 
+      
+         
+         update member
+      set
+      m_date=m_date+1
+      where m_id= 'test1' 
--- DreamUp/src/com/dreamup/member/dao/MemberDAO.java
+++ DreamUp/src/com/dreamup/member/dao/MemberDAO.java
@@ -8,209 +8,213 @@ import com.dreamup.member.dto.MemberDTO;
 import com.ibatis.sqlmap.client.SqlMapClient;
 
 public class MemberDAO {
-	SqlMapClient sqlMap;
+   SqlMapClient sqlMap;
 
-	public MemberDAO() {
-		sqlMap = SqlMapConfig.getSqlMapInstance();
-	}
+   public MemberDAO() {
+      sqlMap = SqlMapConfig.getSqlMapInstance();
+   }
+   //회원가입ok
+   public boolean insert(MemberDTO member) {
+      boolean result = false;
+      try {
+         System.out.println("before insert to member : " + member.toString());
+         sqlMap.insert("member.insert", member);
 
-	// 회원가입 : 다운 ok
-	public boolean insert(MemberDTO member) {
-		boolean result = false;
-		try {
-			System.out.println("before insert to member : " + member.toString());
-			sqlMap.insert("member.insert", member);
+         result = true;
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+      return result;
+   }
+   // 비밀번호 암호화
+      public boolean encryptionPwd(String m_password) {
+         int result;
+         try {
+            result = (int) sqlMap.queryForObject("member.encryptionPwd", m_password);
+         } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }
+         return false;
+      }
+      // 회원탈퇴 : 다운ok
+      public boolean delete(String m_id) {
+         int result;
+         try {
+            result = sqlMap.delete("member.delete", m_id);
+            return true;
+         } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }
+         return false;
+      }
+      // 아이디 찾기  ok
+      public String findId(MemberDTO member) {
 
-			result = true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
+         String m_id = null;
+         try {
+            m_id = (String) sqlMap.queryForObject("member.findId", member);
+            if(m_id != null){
+               return m_id;
+            }else{
+               System.out.println("이름, 메일 입력값을 확인하세요");
+            }
+         } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }
 
-	// 비밀번호 암호화
-	public boolean encryptionPwd(String m_password) {
-		int result;
-		try {
-			result = (int) sqlMap.queryForObject("member.encryptionPwd", m_password);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	// 회원탈퇴 : 다운ok
-	public boolean delete(String m_id) {
-		int result;
-		try {
-			result = sqlMap.delete("member.delete", m_id);
-			return true;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	// 아이디 찾기  ok
-	public String findId(MemberDTO member) {
-
-		String m_id = null;
-		try {
-			m_id = (String) sqlMap.queryForObject("member.findId", member);
-			if(m_id != null){
-				return m_id;
-			}else{
-				System.out.println("이름, 메일 입력값을 확인하세요");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return null;
-	}
-
-	// 비밀번호 재설정 : 다운ok
-	public boolean resetPwd(MemberDTO member) {
-		int result;
-		try {
-			result = sqlMap.update("member.resetPwd", member);
-			if (result == 1) {
-				return true;
-			}else{
-				System.out.println("이름, 메일, 아이디 입력값을 확인하세요");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	// 중복검사 ok
-	public boolean IdDuplicationCheck(String m_id) {
-		int result;
-		try {
-			result = (int) sqlMap.queryForObject("member.IdDuplicationCheck", m_id);
-			if(result == 0){
-				System.out.println("사용가능한 아이디 입니다.");
-			return true;
-			}else {
-				System.out.println("이미 존재하는 아이디 입니다.\n아이디를 다시 입력해주세요.");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	// 로그인 ok
-	public boolean login(MemberDTO member) {
-		int result;
-		try {
-			result = (int) sqlMap.queryForObject("member.login", member);
-			if (result == 1) {
-				System.out.println("로그인 성공");
-				return true;
-			}else{
-				//아이디 불일치 or 비번 불일치는 유효성검사인가?
-				System.out.println("아이디와 비밀번호를 다시 입력해주세요");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	// 한번이라도 후원한 적이 있는 사용자가 배송주소를 당사자 주소지로 할 경우
-	public boolean supportingCount(String m_id) {
-		int result;
-		try {
-			result = (int) sqlMap.queryForObject("member.supportingCount", m_id);
-			if(result==0){
-				
-			return true;
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	// 사용자 후원정보 입력시 가입정보가 본인인경우
-	public MemberDTO selectOwn(String m_id) {
-		MemberDTO member;
-		try {
-			member = (MemberDTO) sqlMap.queryForObject("member.selectOwn", m_id);
-			return member;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	// 마이페이지 개인 정보 수정 ok
-	public boolean updateInfo(MemberDTO member) {
-		int result;
-		try {
-			result = sqlMap.update("member.updateInfo", member);
-		if (result == 1) {
-			return true;
-		  }else{
-			  System.out.println("아이디가 일치하지 않습니다.");
-		  }
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-	
-	// 한번도 후원한 적이 없는 사용자가 사용자 후원정보 입력시 배송주소를 당사자 주소지로 할경우
-	public boolean updateSupport(String m_id) {
-		int result;
-		try {
-			result = sqlMap.update("member.updateSupport", m_id);
-			return true;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-	
-	// project테이블/support테이블에 속하는 멤버 컬럼의 값을 가져오기 위함
-	// 사용자 세션 부여시
-	public MemberDTO selectMember(int i) {
-		MemberDTO member;
-
-		try {
-			member = (MemberDTO) sqlMap.queryForObject("member.selectMember", i);
-			return member;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	// 전체 유저 정보 조회 ok
-	public List<MemberDTO> listMember() {
-		List<MemberDTO> memberList;
-		try {
-			memberList = sqlMap.queryForList("member.listMember");
-			return memberList;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
+         return null;
+      }
+      // 비밀번호 재설정 : 다운ok
+      public boolean resetPwd(MemberDTO member) {
+         int result;
+         try {
+            result = sqlMap.update("member.resetPwd", member);
+            if (result == 1) {
+               return true;
+            }else{
+               System.out.println("이름, 메일, 아이디 입력값을 확인하세요");
+            }
+         } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }
+         return false;
+      }
+      // 중복검사 ok
+   public boolean IdDuplicationCheck(String m_id){
+      try {
+         int result;
+         result = (int) sqlMap.queryForObject("member.IdDuplicationCheck",m_id);
+         if(result == 0){
+            System.out.println("사용가능한 아이디 입니다.");
+         return true;
+         }
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return false;
+   }
+   // 로그인 ok
+   public boolean login(MemberDTO member){
+      try {
+         sqlMap.queryForObject("member.login",member);
+         return true;
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return false;
+   }
+   // 한번도 후원한 적이 없는 사용자가 사용자 후원정보 입력시 배송주소를 당사자 주소지로 할경우
+   public boolean supportingCount(String m_id){
+      try {
+         int result;
+         result = (int) sqlMap.queryForObject("member.supportingCount",m_id);
+         if(result==0){
+            
+            return true;
+         }
+         
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return false;
+   }
+   // 사용자 후원정보 입력시 가입정보가 본인인경우
+   public MemberDTO selectOwn(String m_id){
+      try {
+         MemberDTO result;
+         result = (MemberDTO) sqlMap.queryForObject("member.selectOwn",m_id);
+         return result;
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return null;
+   }
+   // 마이페이지 개인 정보 수정 ok
+   public boolean updateInfo(MemberDTO member){
+      try {
+         int result;
+         result = sqlMap.update("member.updateInfo",member);
+         if(result == 1){
+         return true;
+         }
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return false;
+   }
+   //한번도 후원한 적이 없는 사용자가 사용자 후원정보 입력시 배송주소를 당사자 주소지로 할경우 ok
+   public boolean updateSupport(MemberDTO member){
+      try {
+         int result;
+         result = sqlMap.update("member.updateSupport",member);
+         if(result == 1){
+         return true;
+         }
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return false;
+   }
+   // project테이블/support테이블에 속하는 멤버 컬럼의 값을 가져오기 위함
+      // 사용자 세션 부여시
+   public MemberDTO selectMember(int m_no){
+      try {
+         MemberDTO result;
+         result = (MemberDTO) sqlMap.queryForObject("member.selectMember",m_no);
+         return result;
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return null;
+   }
+   // 전체 유저 정보 조회 ok
+   public List<MemberDTO> listMember(MemberDTO member){
+      try {
+         List list;
+         list = sqlMap.queryForList("member.listMember",member);
+         return list;
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return null;
+   }
+   //내가 후원하는 프로젝트 카운트 
+   public boolean addSupportingCount(String m_id){
+      int result;
+      try {
+         result = sqlMap.update("member.addSupportingCount", m_id);
+         if(result == 1){
+         return true;
+         }
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block.9
+         e.printStackTrace();
+      }
+      return false;
+   }
+   
+   public boolean updateSupportedCount(MemberDTO member){
+      int result;
+      try {
+         result = sqlMap.update("member.updateSupportedCount", member);
+         return true;
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block.9
+         e.printStackTrace();
+      }
+      return false;
+   }
 
 }
